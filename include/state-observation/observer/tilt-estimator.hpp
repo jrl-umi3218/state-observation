84078e68d9144a472901e7dcf05bc1aa0a28dde6
ArnaudDmt <dem.arnaud11@gmail.com>
2023-10-24 17:56:10 +0900

    Separated version of TiltEstimator for humanoids

--- include/state-observation/observer/tilt-estimator.hpp
+++ include/state-observation/observer/tilt-estimator.hpp
@@ -84,94 +84,27 @@ public:
     return dt_;
   }
 
-  /// sets the position of the IMU sensor in the control frame
-  void setSensorPositionInC(const Vector3 & p)
-  {
-    p_S_C_ = p;
-  }
-  Vector3 getSensorPositionInC()
-  {
-    return p_S_C_;
-  }
-
-  /// sets the oriantation of the IMU sensor in the control frame
-  void setSensorOrientationInC(const Matrix3 & R)
-  {
-    R_S_C_ = R;
-  }
-  Matrix3 getSensorOrientationInC()
-  {
-    return R_S_C_;
-  }
-
-  Vector3 getVirtualLocalVelocityMeasurement()
-  {
-    return x1_;
-  }
-
-  /// sets teh linear velocity of the IMU sensor in the control frame
-  void setSensorLinearVelocityInC(const Vector3 & v)
-  {
-    v_S_C_ = v;
-  }
-
-  Vector3 getSensorLinearVelocityInC()
-  {
-    return v_S_C_;
-  }
-
-  /// sets the angular velocity of the IMU sensor in the control frame
-  void setSensorAngularVelocityInC(const Vector3 & w)
-  {
-    w_S_C_ = w;
-  }
-  Vector3 getSensorAngularVelocityInC()
-  {
-    return w_S_C_;
-  }
-
-  /// sets the velocity of the control origin in the world frame
-  /// this velocity has to be expressed in the control frame.
-  void setControlOriginVelocityInW(const Vector3 & v)
-  {
-    v_C_ = v;
-  }
-  Vector3 getControlOriginVelocityInW()
-  {
-    return v_C_;
-  }
-
-  /// @brief Sets x1 (the local linear velocity of the IMU in the world) explicitely for the next iteration. Otherwise
-  /// it is computed from the input variables.
-  /// @param x1 the input local linear velocity of the IMU in the world.
-  void setExplicitImuLocVel(const Vector3 & x1);
-
-  /// @brief informs the estimator that x1hat (the estimate of the local linear velocity of the IMU in the world) needs
-  /// to be reset.
-  /// @copydetails checkResetX1hat()
-  void resetImuLocVelHat();
-
-/// prevent c++ overloaded virtual function warning
-#if defined(__clang__)
-#  pragma clang diagnostic push
-#  pragma clang diagnostic ignored "-Woverloaded-virtual"
-#else
-#  if defined(__GNUC__)
-#    pragma GCC diagnostic push
-#    pragma GCC diagnostic ignored "-Woverloaded-virtual"
-#  endif
-#endif
+  /// prevent c++ overloaded virtual function warning
+  // #if defined(__clang__)
+  // #  pragma clang diagnostic push
+  // #  pragma clang diagnostic ignored "-Woverloaded-virtual"
+  // #else
+  // #  if defined(__GNUC__)
+  // #    pragma GCC diagnostic push
+  // #    pragma GCC diagnostic ignored "-Woverloaded-virtual"
+  // #  endif
+  // #endif
 
   /// sets ths measurement (accelero and gyro stacked in one vector)
-  void setMeasurement(const Vector3 ya_k, const Vector3 yg_k, TimeIndex k);
+  void setMeasurement(const Vector3 & yv_k, const Vector3 & ya_k, const Vector3 & yg_k, TimeIndex k);
 
-#if defined(__clang__)
-#  pragma clang diagnostic pop
-#else
-#  if defined(__GNUC__)
-#    pragma GCC diagnostic pop
-#  endif
-#endif
+  // #if defined(__clang__)
+  // #  pragma clang diagnostic pop
+  // #else
+  // #  if defined(__GNUC__)
+  // #    pragma GCC diagnostic pop
+  // #  endif
+  // #endif
 
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
@@ -183,24 +116,6 @@ protected:
   /// Sampling time
   double dt_;
 
-  /// Position of the IMU in the control frame
-  Vector3 p_S_C_;
-
-  /// Orientation of the IMU in the control frame
-  Matrix3 R_S_C_;
-
-  /// Linear velocity of the IMU in the control frame
-  Vector3 v_S_C_;
-
-  /// Angular velocity of the IMU in the control frame
-  Vector3 w_S_C_;
-
-  /// Linear velocity of the control frame
-  Vector3 v_C_;
-
-  /// indicates that x1hat needs to be reset
-  bool resetX1hat_ = false;
-
   /// variables used for the computation
   Vector3 x1_;
   Vector3 x1_hat_;
@@ -208,15 +123,8 @@ protected:
   Vector3 x2_hat_;
   Vector3 dx1_hat;
 
-  bool withExplicitX1_;
-
   /// The tilt estimator loop
   StateVector oneStepEstimation_();
-
-  /// @brief Checks if x1hat needs to be reset and if yes, resets it with the current value of x1.
-  /// @details x1hat needs to be reset when the mode of computation of the anchor frame changed, to avoid
-  /// discontinuities.
-  void checkResetX1hat();
 };
 
 } // namespace stateObservation
--- src/tilt-estimator.cpp
+++ src/tilt-estimator.cpp
@@ -4,8 +4,7 @@ namespace stateObservation
 {
 
 TiltEstimator::TiltEstimator(double alpha, double beta, double gamma)
-: ZeroDelayObserver(9, 6), alpha_(alpha), beta_(beta), gamma_(gamma), dt_(0.005), p_S_C_(Vector3::Zero()),
-  R_S_C_(Matrix3::Identity()), v_S_C_(Vector3::Zero()), w_S_C_(Vector3::Zero()), v_C_(Vector3::Zero())
+: ZeroDelayObserver(9, 9), alpha_(alpha), beta_(beta), gamma_(gamma), dt_(0.005)
 {
 }
 
@@ -20,32 +19,12 @@ void TiltEstimator::initEstimator(Vector3 x1, Vector3 x2_prime, Vector3 x2)
   setState(initStateVector, 0);
 }
 
-void TiltEstimator::setMeasurement(const Vector3 ya_k, const Vector3 yg_k, TimeIndex k)
+void TiltEstimator::setMeasurement(const Vector3 & yv_k, const Vector3 & ya_k, const Vector3 & yg_k, TimeIndex k)
 {
-  ObserverBase::MeasureVector y_k(6);
-  y_k << ya_k, yg_k;
+  ObserverBase::MeasureVector y_k(9);
+  y_k << yv_k, ya_k, yg_k;
 
   ZeroDelayObserver::setMeasurement(y_k, k);
-}
-
-void TiltEstimator::setExplicitImuLocVel(const Vector3 & x1)
-{
-  withExplicitX1_ = true;
-  x1_ = x1;
-}
-
-void TiltEstimator::resetImuLocVelHat()
-{
-  resetX1hat_ = true;
-}
-
-void TiltEstimator::checkResetX1hat()
-{
-  if(resetX1hat_)
-  {
-    x_().segment<3>(0) = x1_;
-    resetX1hat_ = false;
-  }
 }
 
 ObserverBase::StateVector TiltEstimator::oneStepEstimation_()
@@ -54,19 +33,9 @@ ObserverBase::StateVector TiltEstimator::oneStepEstimation_()
 
   BOOST_ASSERT(this->y_.size() > 0 && this->y_.checkIndex(k + 1) && "ERROR: The measurement vector is not set");
 
-  Vector3 ya = getMeasurement(k + 1).head<3>();
+  Vector3 yv = getMeasurement(k + 1).head<3>();
+  Vector3 ya = getMeasurement(k + 1).segment<3>(3);
   Vector3 yg = getMeasurement(k + 1).tail<3>();
-
-  if(!withExplicitX1_)
-  {
-    x1_ = R_S_C_.transpose() * (v_C_ + v_S_C_) + (yg - R_S_C_.transpose() * w_S_C_).cross(R_S_C_.transpose() * p_S_C_);
-  }
-  else
-  {
-    withExplicitX1_ = false;
-  }
-
-  checkResetX1hat();
 
   ObserverBase::StateVector x_hat = getCurrentEstimatedState();
   x1_hat_ = x_hat.segment<3>(0);
@@ -74,8 +43,8 @@ ObserverBase::StateVector TiltEstimator::oneStepEstimation_()
   x2_hat_ = x_hat.segment<3>(6);
 
   Vector dx_hat(9);
-  dx_hat.segment<3>(0) = x1_hat_.cross(yg) - cst::gravityConstant * x2_hat_prime_ + ya + alpha_ * (x1_ - x1_hat_);
-  dx_hat.segment<3>(3) = x2_hat_prime_.cross(yg) - beta_ * (x1_ - x1_hat_);
+  dx_hat.segment<3>(0) = x1_hat_.cross(yg) - cst::gravityConstant * x2_hat_prime_ + ya + alpha_ * (yv - x1_hat_);
+  dx_hat.segment<3>(3) = x2_hat_prime_.cross(yg) - beta_ * (yv - x1_hat_);
   dx_hat.segment<3>(6) = x2_hat_.cross(yg - gamma_ * x2_hat_.cross(x2_hat_prime_));
 
   x_hat += dx_hat * dt_;
