bd0bdf6226004f52fab66179ebb3cf574d7aba45
ArnaudDmt <dem.arnaud11@gmail.com>
2023-11-02 17:14:52 +0900

    remove debug functions

--- src/extended-kalman-filter.cpp
+++ src/extended-kalman-filter.cpp
@@ -142,13 +142,6 @@ must set directInputOutputFeedthrough to 'false' in the constructor");
   return f_->measureDynamics(x, opt.u_, k);
 }
 
-Matrix displayVectorWithIndex(const Vector & vec1, const Vector & indexes) // to be removed
-{
-  Matrix C(vec1.rows(), vec1.cols() + indexes.cols());
-  C << vec1, indexes;
-  return C;
-}
-
 KalmanFilterBase::Amatrix // ExtendedKalmanFilter<n,m,p>::Amatrix does not work
     ExtendedKalmanFilter::getAMatrixFD(const Vector & dx)
 {
@@ -165,13 +158,6 @@ KalmanFilterBase::Amatrix // ExtendedKalmanFilter<n,m,p>::Amatrix does not work
     else
       opt.u_ = inputVectorZero();
   }
-  const int nbIndexes = 48;
-  Eigen::VectorXd indexes(nbIndexes); // to be removed
-  // Eigen::VectorXd indexes(57); //to be removed
-  for(int ind = 0; ind < indexes.size(); ind++)
-  {
-    indexes(ind) = ind;
-  }
   for(Index i = 0; i < nt_; ++i)
   {
     // std::cout << std::endl << "col: " << std::endl << i << std::endl;
@@ -186,30 +172,8 @@ KalmanFilterBase::Amatrix // ExtendedKalmanFilter<n,m,p>::Amatrix does not work
 
     opt.dx_ /= dx[i];
 
-    bool stopTest = false;
-    for(int j = 0; j < nt_; j++)
-    {
-
-      if(opt.dx_.coeff(j) > 1e+30)
-      {
-        // std::cout << std::endl << "error indexes: " << std::endl << "(" << j << "," << i << ")" << std::endl;
-        // BOOST_ASSERT(false && "error on A");
-        stopTest = true;
-      }
-    }
-    // BOOST_ASSERT(!stopTest && "Erreurs sur A");
-
     opt.a_.col(i) = opt.dx_;
   }
-  Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
-  Matrix sumA;
-  sumA = Matrix::Zero(nt_, nt_);
-  for(Index i = 0; i < nt_; i += 3) // to be deleted
-  {
-    // sumA(i,i) = opt.a_.block<3,3>(i, i).mean();
-  }
-  // std::cout << std::endl << "A compact: " << std::endl << sumA.format(CleanFmt) << std::endl;
-
   return opt.a_;
 }
 
