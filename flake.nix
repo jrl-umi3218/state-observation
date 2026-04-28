cacda2accf67b43dce7203e8779d429bc9f818af
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-04-28 12:01:10 +0200

    style: nix fmt

--- flake.nix
+++ flake.nix
@@ -8,12 +8,20 @@
     inputs.mc-rtc-nix.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        overrideAttrs.state-observation = {drv-prev, pkgs-final, ...}: {
-          src = lib.cleanSource ./.;
-          nativeBuildInputs = [ pkgs-final.doxygen pkgs-final.graphviz pkgs-final.gbenchmark pkgs-final.jrl-cmakemodulesv2 ] ++ drv-prev.nativeBuildInputs;
-          cmakeFlags = [ (lib.cmakeBool "INSTALL_DOCUMENTATION" true) ] ++ drv-prev.cmakeFlags;
-          doCheck = true;
-        };
+        overrideAttrs.state-observation =
+          { drv-prev, pkgs-final, ... }:
+          {
+            src = lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs-final.doxygen
+              pkgs-final.graphviz
+              pkgs-final.gbenchmark
+              pkgs-final.jrl-cmakemodulesv2
+            ]
+            ++ drv-prev.nativeBuildInputs;
+            cmakeFlags = [ (lib.cmakeBool "INSTALL_DOCUMENTATION" true) ] ++ drv-prev.cmakeFlags;
+            doCheck = true;
+          };
       }
     );
 }
